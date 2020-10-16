1d7e5f79e71d60ec5926430c1d0903bec06e6b47
jiaoxiangyu <1062100557@qq.com>
2020-10-16 17:56:35 +0800

    init

---
+++ src/main/java/cn/lookk/zd/ZookeeperDubboSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cn.lookk.zd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperDubboSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperDubboSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lookk/zd/ZookeeperDubboSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lookk.zd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperDubboSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
